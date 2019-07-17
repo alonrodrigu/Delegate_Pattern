94b91b941f228bd29170e316b7b27f0eb2070a5e
Alonzo Rodriguez <alon.rodrigu@gmail.com>
2019-07-17 10:07:00 -0700

    Added textFieldDidBeginEditing delegate method to delegate-fun branch

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -32,10 +32,7 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
             return true
         }
     }
-    // DELEGATE METHOD : textFieldDidBeginEditing - is called when the user selects the text field
-    // TODO: Add and modify the method to build expectation for the output by changing the celsiusLabel when the input field is selected
-    // modify the celsiusLabel text to be a single question mark
-    // modify the celsiusLabel color to be 60% red, 60% green, and 40% blue (refer to the Developer Documentation for UIColor)
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
         celsiusLabel.text = "?"
         celsiusLabel.textColor = UIColor(red: 0.6, green: 0.6, blue: 0.4, alpha: 1.0)
